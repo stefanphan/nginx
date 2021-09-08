b3c3bfe2367d6a2d1a3b9821d2bdbe21fc018155
admin <cloud_service@mirattery.com>
2021-09-08 11:56:36 +0800

    add 2files

---
+++ nginx_bass_dist.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#PROJ_PATH=/appset
+#ip=10.0.0.7
+expect -c "
+  spawn scp -r $PROJ_PATH/nginx/target/dist.tar root@$ip:/appset/nginx/html/baas
+  expect {
+    \"*password*\" {set timeout 300; send \"weineng0528@@\r\";}
+  }
+expect eof"
