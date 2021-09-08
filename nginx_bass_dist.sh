2bc1406f5e19cee457c210e8f8047203cc49d6be
Your Name <you@example.com>
2021-09-08 14:52:59 +0800

    m sh

--- nginx_bass_dist.sh
+++ nginx_bass_dist.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
-#PROJ_PATH=/appset
-#ip=10.0.0.7
 expect -c "
-  spawn scp -r $PROJ_PATH/nginx/dist root@$ip:/appset/nginx/html/baas
+  spawn scp -r $PROJ_PATH/nginx/dist root@$ip_1:/appset/nginx/html/baas
   expect {
-    \"*password*\" {set timeout 300; send \"weineng0528@@\r\";}
+    \"*password*\" {set timeout 300; send \"swcf2fY@\r\";}
+  }
+expect eof"
+expect -c "
+  spawn scp -r $PROJ_PATH/nginx/dist root@$ip_2:/appset/nginx/html/baas
+  expect {
+    \"*password*\" {set timeout 300; send \"swcf2fY@\r\";}
   }
 expect eof"
